0ced88c5102434b534a5b6326cd35750e29236e7
GBNOTEBOOK\capta <captain9697@naver.com>
2022-01-20 20:07:27 +0900

    년도와 월을 입력받아 자동으로 달력 구현

--- src/gb/calendar/Calendar.java
+++ src/gb/calendar/Calendar.java
@@ -26,10 +26,12 @@ public class Calendar {
 
 	}
 
-	public void printCalendar(int year, int month, int weekday) {
+	public void printCalendar(int year, int month) {
 		System.out.printf("   <<%4d년 %2d월>>%n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA ");
 		System.out.println("---------------------");
+		
+		int weekday = getweekDay(year, month, 1);
 
 		// print blank
 		for (int i = 0; i < weekday; i++) {
@@ -53,6 +55,29 @@ public class Calendar {
 			}
 		}
 		System.out.println();
+	}
+
+	private int getweekDay(int year, int month, int day) {
+		int syear = 1583;
+		final int STANDARD_WEEKDAY = 6;
+		
+		int count_day = 0;
+		
+		for(int i = syear; i < year; i++) {
+		  int delta = isLeapYear(i) ? 366 : 365;
+		  count_day += delta;
+		}
+		
+		for(int i = 1; i < month; i++) {
+		  int delta = getMaxDaysOfMonth(year, i);
+		  count_day += delta;
+		}
+		
+		count_day += day - 1;
+		
+		int weekday = (count_day + STANDARD_WEEKDAY) % 7;
+		return weekday;
+		
 	}
 
 }
--- src/gb/calendar/Prompt.java
+++ src/gb/calendar/Prompt.java
@@ -31,7 +31,7 @@ public class Prompt {
 
 		int year = 2021;
 		int month = 1;
-		int weekday = 0;
+	
 		while (true) {
 			System.out.println("년도를 입력하세요.(exit: -1)");
 			System.out.print("YEAR> ");
@@ -50,12 +50,7 @@ public class Prompt {
 				continue;
 			}
 
-			System.out.println("첫번째 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA)");
-			System.out.print("WEEKDAY> ");
-			String str_weekday = scanner.next();
-			weekday = parseDay(str_weekday);
-
-			cal.printCalendar(year, month, weekday);
+			cal.printCalendar(year, month);
 			System.out.println();
 
 		}
